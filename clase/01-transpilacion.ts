05cfe7114732aec9aa114cd2df73e6a88c5a4716
JenniferMelissaRussiCastillo <Jennifer.russi@hotmail.com>
2023-07-01 07:52:50 -0500

    Primer comit

---
+++ clase/01-transpilacion.ts
@@ -0,0 +1,13 @@
+class holamundo
+{
+    varAtributo:string= "";
+    varNumber:number = 0;
+}
+
+(()=>{
+    1>0 ? 'hola':'adios';
+    let variableNumerica:number;
+    variableNumerica = 1 -1;
+}
+)()
+console.log("hola mundo");
---
+++ dist/01-transpilacion.js
@@ -0,0 +1,13 @@
+"use strict";
+class holamundo {
+    constructor() {
+        this.varAtributo = "";
+        this.varNumber = 0;
+    }
+}
+(() => {
+    1 > 0 ? 'hola' : 'adios';
+    let variableNumerica;
+    variableNumerica = 1 - 1;
+})();
+console.log("hola mundo");
